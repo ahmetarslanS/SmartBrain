dd45ecbf9f2649e2c426f6b5fecb66c6b30f3c23
Ahmet Arslan <ahmet_1285_arslan@hotmail.com>
2024-10-30 14:08:56 +0300

    Add Sign in, Register, Sign out and routing.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,149 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import './App.css';
 
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = 'c98a5808a0914fa1adaeda4a2feb6fb0';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'ahmetarslan';
+  const APP_ID = 'SmartBrain';
+  // Change these to whatever model and image URL you want to use
+  //const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions.region_info.bounding_box
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(this.state.input))
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)/*response.json()*/ ))
+    // .then(result => {
+
+    //     const regions = result.outputs[0].data.regions;
+
+    //     regions.forEach(region => {
+    //         // Accessing and rounding the bounding box values
+    //         const boundingBox = region.region_info.bounding_box;
+    //         const topRow = boundingBox.top_row.toFixed(3);
+    //         const leftCol = boundingBox.left_col.toFixed(3);
+    //         const bottomRow = boundingBox.bottom_row.toFixed(3);
+    //         const rightCol = boundingBox.right_col.toFixed(3);
+
+    //         region.data.concepts.forEach(concept => {
+    //             // Accessing and rounding the concept value
+    //             const name = concept.name;
+    //             const value = concept.value.toFixed(4);
+
+    //             console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+    //         });
+    //     });
+
+    // })
+    .catch(error => console.log('error', error));
+
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false});
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-      <ParticlesBg type="square" num={25} bg={true} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-          {/* <FaceRecognition /> */}
+        <ParticlesBg type="square" num={25} bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ?  <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+              route === 'signin' 
+              ? <Signin onRouteChange={this.onRouteChange} /> 
+              : <Register onRouteChange={this.onRouteChange} />
+            )
+        }
       </div>
     );
   }
